635efdc8f98ce78024895c5948684de952dd0204
Albert Tran <alberdt2@uci.edu>
2019-07-24 01:18:29 +0000

    Added user profiles form

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,5 +3,6 @@ class ProfilesController < ApplicationController
    # GET to /users/:user:id/profile/new
     def new
         # Render blank profile details form
+        @profile = Profile.new
     end
 end
